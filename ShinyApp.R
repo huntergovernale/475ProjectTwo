78ba49b1df9c8f735f2501a50d354a8d17e6fb2c
Hunter Governale <99290914+huntergovernale@users.noreply.github.com>
2022-03-08 16:16:33 -0500

    Update ShinyApp.R

--- ShinyApp.R
+++ ShinyApp.R
@@ -4,22 +4,26 @@ library(ggplot2)
 library(shinyWidgets)
 library(quantmod)
 library(plotly)
+SYMBOLS <- stockSymbols()
+
 
 ui <- fluidPage(
   titlePanel(title = h3("What if I invested?", 
                         align = "center")),
-  
-  br(),
-  
-  textInput("stock", 
-            label = h3("Input a stock that you want to consider"), 
-            value = ". . ."),
-  
-  
+  selectInput("select", label = h3("Select A Stock Name"), 
+           choices = names(table(SYMBOLS$Name)), selected = 1),
+  hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
 )
+  br()
+  
+ textInput("stock", label = h3("Input a stock that you want to consider"), value = ". . .")
+  
+  
+
 
 server <- function(input, output, session) {
-
+output$value <- renderPrint({ SYMBOLS$Symbol[which(SYMBOLS$Name == input$select)] })
   
 }
 
